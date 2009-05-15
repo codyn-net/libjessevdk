cca2ccb7dc7ef666e607a629ddd9f8ee56d62ef9
Jesse van den Kieboom <jvanden@grijpc10.epfl.ch>
2009-05-15 17:21:54 +0200

    Added euclidean distance calculating in math

--- base/Math/math.hh
+++ base/Math/math.hh
@@ -2,6 +2,8 @@
 #define __BASE_MATH_H__
 
 #include <base/Math/random.hh>
+#include <algorithm>
+#include <cmath>
 
 namespace base
 {
@@ -13,6 +15,9 @@ namespace base
 			
 			template <typename T>
 			static T min(T const &a, T const &b);
+			
+			template <typename Container>
+			static typename Container::value_type euclidean(Container const &container);
 	};
 	
 	template <typename T>
@@ -25,6 +30,18 @@ namespace base
 	T Math::min(T const &a, T const &b)
 	{
 		return a < b ? a : b;
+	}
+	
+	template <typename Container>
+	static typename Container::value_type euclidean(Container const &container)
+	{
+		typename Container::const_iterator iter;
+		typename Container::value_type sum;
+		
+		for (iter = container.begin(); iter != container.end(); ++iter)
+			sum += *iter * *iter;
+		
+		return ::sqrt(sum);
 	}
 }
 
