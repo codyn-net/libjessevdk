778ad461afac2f696b7a42e78942fcdb9dd60e58
Jesse van den Kieboom <jesse@icecrew.nl>
2009-07-13 10:38:37 +0200

    Fixed handling different conditions

--- os/os/FileDescriptor/dataoniosource.cc
+++ os/os/FileDescriptor/dataoniosource.cc
@@ -2,36 +2,41 @@
 
 bool FileDescriptor::Data::onIOSource(Glib::IOCondition condition)
 {
-	if (condition == Glib::IO_HUP)
+	if (condition & (Glib::IO_IN | Glib::IO_PRI))
 	{
-		close();
-		return false;
-	}
-	
-	if (onIO(condition))
-		return true;
-	
-	/* try to read something */
-	string buffer;
-	ssize_t len = recv(buffer);
-	
-	if (len < 1)
-	{
-		if (len < 0)
+		if (!onIO(condition))
 		{
-			if (errno == EAGAIN || errno == EWOULDBLOCK)
-				return true;
-		}
+			/* try to read something */
+			string buffer;
+			ssize_t len = recv(buffer);
 
+			if (len < 1)
+			{
+				if (len < 0)
+				{
+					cerr << "*** " << strerror(errno) << endl;
+					if (errno == EAGAIN || errno == EWOULDBLOCK)
+						return true;
+				}
+
+				close();
+				return false;
+			}
+
+			this->buffer.append(buffer);
+	
+			Cloneable<DataArgs> args = createArgs(fd, &(this->buffer));
+			this->buffer = "";
+	
+			onData(*args);
+		}
+	}
+
+	if (condition & (Glib::IO_HUP | Glib::IO_ERR))
+	{
 		close();
 		return false;
 	}
-
-	this->buffer.append(buffer);
 	
-	Cloneable<DataArgs> args = createArgs(fd, &(this->buffer));
-	this->buffer = "";
-	
-	onData(*args);
 	return true;
 }
